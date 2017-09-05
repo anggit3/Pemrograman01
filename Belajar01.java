ab1beebc27810dc9c1947c2878898731f2e3e913
Anggit 3 Hamzah <31609436+anggit3@users.noreply.github.com>
2017-09-04 20:11:28 -0800

    Add files via upload

---
+++ Belajar01.java
@@ -0,0 +1,8 @@
+public class Belajar01{
+
+     public static void main(String []args){
+        System.out.println("\nNama : Anggit Tri Hamzah");
+        System.out.println("Nim : 17220025 \n");
+        
+     }
+}
---
+++ Belajar03.java
@@ -0,0 +1,12 @@
+public class Belajar03{
+
+     public static void main(String []args){
+         int panjang,lebar,luas,keliling;
+         panjang = 10;
+         lebar = 15;
+         luas = panjang * lebar;
+         System.out.println("Luas Persegi Panjang = " + luas);
+         keliling = 2 *  (panjang * lebar);
+         System.out.println("keliling persegi panjang = " + keliling);
+     }
+}
